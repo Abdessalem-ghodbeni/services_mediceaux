74c1fb05d61c3f59a49569ecf7fc97784e083dc0
Abdessalem-ghodbeni <slouma4ghodbeny@gmail.com>
2023-12-10 21:18:26 +0100

    mise en place dependance

---
+++ src/main/java/com/example/medicaux_service/MedicauxServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicaux_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicauxServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicauxServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/medicaux_service/MedicauxServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicaux_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicauxServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
